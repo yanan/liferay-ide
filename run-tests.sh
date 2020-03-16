4b82b86c9a06e8227d755bcd132710b1e69a7a81
Ashley Yuan <yanan.yuan@liferay.com>
2020-03-16 17:36:53 +0800

    clean and remove soe caches

--- run-tests.sh
+++ run-tests.sh
@@ -14,6 +14,10 @@ function checkError {
 	fi
 }
 
-./mvnw --fail-at-end -e -Dmaven.repo.local=${GITHUB_WORKSPACE}/liferay-ide-m2-repository verify -Dliferay.bundles.dir="./tests-resources"
+rm -rf /home/runner/work/liferay-ide/liferay-ide/liferay-ide-m2-repository/.cache/tycho
+echo 00000
+rm -rf /home/runner/work/liferay-ide/liferay-ide/liferay-ide-m2-repository/p2/osgi/bundle
+echo 11111
+./mvnw clean --fail-at-end -e -Dmaven.repo.local=${GITHUB_WORKSPACE}/liferay-ide-m2-repository verify -Dliferay.bundles.dir="./tests-resources"
 
 checkError
