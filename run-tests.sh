a446295a8f836eedbfbce9fc499b84a5620081f2
Ashley Yuan <yanan.yuan@liferay.com>
2020-12-17 14:07:29 +0800

    test2

--- run-tests.sh
+++ run-tests.sh
@@ -23,8 +23,9 @@ sudo sysctl -w fs.file-max=5000000
 ulimit -Hn
 ulimit -Hn
 cat /proc/sys/fs/file-max
-
 echo 1111
+sudo sysctl kernel.pid_max=126820
+echo 2222
 cat /proc/self/limits
 ./mvnw clean --fail-at-end -e -Dmaven.repo.local=${GITHUB_WORKSPACE}/liferay-ide-m2-repository verify -Dliferay.bundles.dir="./tests-resources"
 
