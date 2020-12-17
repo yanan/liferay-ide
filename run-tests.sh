d39b142956a5f0c1cafdd79c290a2e142c577407
Ashley Yuan <yanan.yuan@liferay.com>
2020-12-17 10:16:18 +0800

    increase open file limit

--- run-tests.sh
+++ run-tests.sh
@@ -14,6 +14,11 @@ function checkError {
 	fi
 }
 
+cat /proc/sys/fs/file-max
+ulimit -Hn
+ulimit -Hn
+
+sudo sysctl -w fs.file-max=3245643
 ./mvnw clean --fail-at-end -e -Dmaven.repo.local=${GITHUB_WORKSPACE}/liferay-ide-m2-repository verify -Dliferay.bundles.dir="./tests-resources"
 
 checkError
