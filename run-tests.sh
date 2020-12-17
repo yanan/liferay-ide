c5075cc1d552e203fc09d007af8e56422ed83c85
Ashley Yuan <yanan.yuan@liferay.com>
2020-12-17 11:00:31 +0800

    test1

--- run-tests.sh
+++ run-tests.sh
@@ -22,6 +22,10 @@ ulimit -Hn
 sudo sysctl -w fs.file-max=5000000
 ulimit -Hn
 ulimit -Hn
+cat /proc/sys/fs/file-max
+
+echo 1111
+cat /proc/self/limits
 ./mvnw clean --fail-at-end -e -Dmaven.repo.local=${GITHUB_WORKSPACE}/liferay-ide-m2-repository verify -Dliferay.bundles.dir="./tests-resources"
 
 checkError
